b354c75d0178c9f10df4b00b30a0e9a52688e84a
Diceekey <diceekey@gmail.com>
2024-11-10 21:59:30 +0700

    Add files via upload

---
+++ accounts.js
@@ -0,0 +1,15 @@
+// accounts.js
+module.exports = [
+    {
+        "email": "isi_dengan_email",
+        "token": "isi_dengan_token"
+      },
+      {
+        "email": "isi_dengan_email",
+        "token": "isi_dengan_token"
+      },
+      {
+        "email": "isi_dengan_email",
+        "token": "isi_dengan_token"
+      }
+];
